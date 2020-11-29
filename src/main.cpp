eae8a53c36d764ebaf30ea68ba16187b8481c148
DanialHanif <danialhanif@outlook.com>
2020-11-29 13:08:19 +0800

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,10 @@
 #include <GL/glu.h>
 #include <GL/gl.h>
 #endif
-#include <time.h>
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
-#define PI 3.1415926535897932384626433832795
+#define PI 3.14159265
 #define FPS 60
 
 double X = 0;
@@ -26,6 +25,11 @@ double dy = 3.0;
 
 int frame = 0; //stores current frame count
 
+/* 
+========================
+Drawing tools
+========================
+ */
 void nGon(int x, int y, int r, int n) {
 	double inc = (2 * PI) / n;
 	glBegin(GL_POLYGON);
@@ -44,45 +48,35 @@ void rectangle(double x, double y, double w, double h) {
 	glEnd();
 }
 
-void house(double x, double y, double w) {
+/* 
+========================
+Drawing tools
+========================
+ */
 
-	double h = w * (2.0 / 3.0);
+/* 
+========================
+Objects
+========================
+ */
 
-	// draw body
-	glColor3ub(200, 50, 0);
-	rectangle(x, y, w, h);
 
-	// draw door
-	glColor3ub(255, 250, 0);
-	rectangle(x+w/2 - w * 0.1, y, w * 0.2, w * (2.0 / 6.0));
 
-	// draw left window
-	glColor3ub(50, 250, 255);
-	rectangle(x + w * 0.125, y + w * (2.0 / 6.0), w * (2.0 / 9.0), w * (2.0 / 9.0));
 
-	// draw right window
-	glColor3ub(50, 250, 255);
-	rectangle(x + w - (w * 0.125) - w * (2.0 / 9.0), y + w * (2.0 / 6.0), w * (2.0 / 9.0), w * (2.0 / 9.0));
 
-	// draw roof
-	glColor3ub(0, 250, 15);
-	glBegin(GL_POLYGON);
-		glVertex2d(x-(w/6.0), y+h);
-		glVertex2d(x + w + (w / 6.0), y+h);
-		glVertex2d(x + w, y + h + (h/3.0));
-		glVertex2d(x, y + h + (h / 3.0));
-	glEnd();
+/* 
+========================
+Objects
+========================
+ */
 
-}
+/* 
+========================
+Scenes
+========================
+ */
 
 void scene_1(){
-
-	X += dx;
-	Y += dy;
-
-	if (X<0 || X > SCREEN_WIDTH) dx *= -1;
-	if (Y<0 || Y > SCREEN_HEIGHT) dy *= -1;
-	house(X, Y, 100);
 
 }
 
@@ -116,6 +110,12 @@ void scene_10(){
 void scene_11(){
 
 }
+
+/* 
+========================
+Scenes
+========================
+ */
 
 void display() {
 	frame++;
